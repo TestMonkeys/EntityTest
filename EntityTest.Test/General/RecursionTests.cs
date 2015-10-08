c03cb5d9d98621a724aaf3bd466f0f6869fa38b7
Pascal Constantin <costea32@gmail.com>
2015-10-08 18:53:34 +0300

    Adding more tests to cover the issue

--- EntityTest.Test/General/RecursionTests.cs
+++ EntityTest.Test/General/RecursionTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using EntityTest.Test.PropertySetValidatorTests.TestObjects;
+﻿using EntityTest.Test.PropertySetValidatorTests;
 using NUnit.Framework;
 using TestMonkeys.EntityTest;
 using TestMonkeys.EntityTest.Framework;
@@ -11,6 +7,96 @@ namespace EntityTest.Test.General
 {
     public class RecursionTests
     {
+        [Test]
+        public void PropertySet_Compare_RecursiveReference_Level0()
+        {
+            var expected = new ObjectWithRecursiveRef {IntValue = 1};
+            expected.RecursiveRef = expected;
+            var actual = new ObjectWithRecursiveRef {IntValue = 1};
+            actual.RecursiveRef = actual;
+            Assert.That(actual, Entity.EqualTo(expected));
+        }
+
+        [Test]
+        public void PropertySet_Compare_RecursiveReference_Negative_ExpectedNull_Level0()
+        {
+            var expected = new ObjectWithRecursiveRef
+            {
+                IntValue = 1,
+                RecursiveRef = null
+            };
+            var actual = new ObjectWithRecursiveRef {IntValue = 1};
+            actual.RecursiveRef = actual;
+            var messageCheck = new MessageCheck("Property Set is not equal");
+            messageCheck.AddPropertyLine("Null", typeof (ObjectWithRecursiveRef).FullName, "RecursiveRef");
+            var ex = Assert.Throws(typeof (AssertionException),
+                () =>
+                    Assert.That(actual, Entity.EqualTo(expected)));
+            messageCheck.Check(ex);
+        }
+
+        [Test]
+        public void PropertySet_Compare_RecursiveReference_Negative_ActualNull_Level0()
+        {
+            var expected = new ObjectWithRecursiveRef {IntValue = 1};
+            expected.RecursiveRef = expected;
+            var actual = new ObjectWithRecursiveRef
+            {
+                IntValue = 1,
+                RecursiveRef = null
+            };
+            var messageCheck = new MessageCheck("Property Set is not equal");
+            messageCheck.AddPropertyLine(typeof (ObjectWithRecursiveRef).FullName, "Null", "RecursiveRef");
+            var ex = Assert.Throws(typeof (AssertionException),
+                () =>
+                    Assert.That(actual, Entity.EqualTo(expected)));
+            messageCheck.Check(ex);
+        }
+
+        [Test]
+        public void PropertySet_Compare_RecursiveReference_Negative_RecursiveOnlyInActual_Level0()
+        {
+            var expected = new ObjectWithRecursiveRef {IntValue = 1};
+            expected.RecursiveRef = new ObjectWithRecursiveRef {IntValue = 2};
+            var actual = new ObjectWithRecursiveRef {IntValue = 1};
+            actual.RecursiveRef = actual;
+            var messageCheck = new MessageCheck("Property Set is not equal");
+            messageCheck.AddPropertyLine("Null", typeof (ObjectWithRecursiveRef).FullName, "RecursiveRef.RecursiveRef");
+            messageCheck.AddPropertyLine("2", "1", "RecursiveRef.IntValue");
+            var ex = Assert.Throws(typeof (AssertionException),
+                () =>
+                    Assert.That(actual, Entity.EqualTo(expected)));
+            messageCheck.Check(ex);
+        }
+
+        [Test]
+        public void PropertySet_Compare_RecursiveReference_Negative_RecursiveOnlyInExpected_Level0()
+        {
+            var expected = new ObjectWithRecursiveRef {IntValue = 1};
+            expected.RecursiveRef = expected;
+            var actual = new ObjectWithRecursiveRef {IntValue = 1};
+            actual.RecursiveRef = new ObjectWithRecursiveRef {IntValue = 2};
+            var messageCheck = new MessageCheck("Property Set is not equal");
+            messageCheck.AddPropertyLine(typeof (ObjectWithRecursiveRef).FullName, "Null", "RecursiveRef.RecursiveRef");
+            messageCheck.AddPropertyLine("1", "2", "RecursiveRef.IntValue");
+            var ex = Assert.Throws(typeof (AssertionException),
+                () =>
+                    Assert.That(actual, Entity.EqualTo(expected)));
+            messageCheck.Check(ex);
+        }
+
+        [Test]
+        public void PropertySet_Compare_RecursiveReference_Level1()
+        {
+            var expected = new ObjectWithRecursiveRef {IntValue = 1};
+            expected.ChildWithRefToParent = new ChildWithRefToParent {ParentRef = expected};
+            var actual = new ObjectWithRecursiveRef {IntValue = 1};
+            actual.ChildWithRefToParent = new ChildWithRefToParent {ParentRef = actual};
+            Assert.That(actual, Entity.EqualTo(expected));
+        }
+
+        
+
         public class ObjectWithRecursiveRef
         {
             [ChildEntity]
@@ -28,25 +114,6 @@ namespace EntityTest.Test.General
             public ObjectWithRecursiveRef ParentRef { get; set; }
         }
 
-        //[Ignore("Recursion Issue")]
-        [Test]
-        public void PropertySet_RecursiveReference_Level0()
-        {
-            var expected = new ObjectWithRecursiveRef {IntValue = 1};
-            expected.RecursiveRef = expected;
-            var actual = new ObjectWithRecursiveRef {IntValue = 1};
-            actual.RecursiveRef = actual;
-            Assert.That(actual,Entity.EqualTo(expected));
-        }
-        //[Ignore("Recursion Issue")]
-        [Test]
-        public void PropertySet_RecursiveReference_Level1()
-        {
-            var expected = new ObjectWithRecursiveRef { IntValue = 1 };
-            expected.ChildWithRefToParent = new ChildWithRefToParent {ParentRef = expected};
-            var actual = new ObjectWithRecursiveRef { IntValue = 1 };
-            actual.ChildWithRefToParent = new ChildWithRefToParent { ParentRef = actual };
-            Assert.That(actual, Entity.EqualTo(expected));
-        }
+      
     }
 }
